ebe8e36c0d0e370c4375a69fbfb7f5b7cd3d57dc
or1is1 <femanifest@gmail.com>
2019-11-07 14:40:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import os
-import src.preprocessing as pre
+import preprocessing as pre
 
 
-imgPath = "./img/"
-dirPath = "./img/chanel/"
-outPath = "./img/_pre/"
-etcPath = "./img/_etc/"
+def preprocess():
+	imgPath = "./img/"
+	dirPath = "./img/chanel/"
+	outPath = "./img/_pre/"
+	etcPath = "./img/_etc/"
 
-pre.saveCroppedImg(dirPath, outPath)
+	pre.saveCroppedImg(dirPath, outPath)
 
-fileList = os.listdir(imgPath)
-folderList = []
+	fileList = os.listdir(imgPath)
+	folderList = []
 
-for file in fileList:
-        if pre.getExt(file) == "csv":
-            folderList.append(file[:-4])
+	for file in fileList:
+	        if pre.getExt(file) == "csv":
+	            folderList.append(file[:-4])
 
-for folderName in folderList:
-	if folderName == "drjart":
-		pre.optimizeFolder(dirPath, etcPath, 1)
-		break
-	pre.saveCroppedImg(folder, path, outPath)
-	# pre.voTTCSV2YOLOAnnoTxt()
+	for folderName in folderList:
+		if folderName == "drjart":
+			pre.optimizeFolder(dirPath, etcPath, 1)
+			break
+		pre.saveCroppedImg(folder, path, outPath)
+		# pre.voTTCSV2YOLOAnnoTxt()
 	pre.plotTrainBoxRatio() # 전체 이미지에서 학습 영역이 차지하는 비율을 구해서 그래프로 그림
+
+preprocess()
